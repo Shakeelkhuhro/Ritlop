4527481020942251afa8907185deaeebe01605b8
Shakeel Khuhro <shakeelahmedkhuhro5454@gmail.com>
2023-05-09 15:31:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// For Search Button functionalty
+/* const form = document.getElementById('search-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const searchTerm = form.elements['search-term'].value;
+
+  // TODO: Add code to perform search with searchTerm
+});
+*/
